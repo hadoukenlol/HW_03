5f2ece8ee54b5f820cc78fa82cee8b5d4850c641
Hadoukenlol <noobs.ololo@gmail.com>
2023-08-14 11:16:12 +0300

    Добавьте файлы проекта.

---
+++ HW_03/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HW_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var students = classes.SelectMany(x => x.Students).ToArray();
+            
+            return students;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
